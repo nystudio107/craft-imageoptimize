8d82323a3d4f6fe6d1a70dff89c2a41d48449dde
Andrew Welch <andrew@nystudio107.com>
2017-10-20 10:04:00 -0400

    Fixed errant spaces in the config.php
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/config.php
+++ src/config.php
@@ -110,46 +110,46 @@ return [
     'imageProcessors'            => [
         // jpeg optimizers
         'jpegoptim' => [
-            'commandPath'           => ' / usr / bin / jpegoptim',
-            'commandOptions'        => ' - s',
+            'commandPath'           => '/usr/bin/jpegoptim',
+            'commandOptions'        => '-s',
             'commandOutputFileFlag' => '',
         ],
         'mozjpeg'   => [
-            'commandPath'           => ' / usr / bin / mozjpeg',
-            'commandOptions'        => ' - optimize - copy none',
-            'commandOutputFileFlag' => ' - outfile',
+            'commandPath'           => '/usr/bin/mozjpeg',
+            'commandOptions'        => '-optimize -copy none',
+            'commandOutputFileFlag' => '-outfile',
         ],
         'jpegtran'  => [
-            'commandPath'           => ' / usr / bin / jpegtran',
-            'commandOptions'        => ' - optimize - copy none',
+            'commandPath'           => '/usr/bin/jpegtran',
+            'commandOptions'        => '-optimize -copy none',
             'commandOutputFileFlag' => '',
         ],
         // png optimizers
         'optipng'   => [
-            'commandPath'           => ' / usr / bin / optipng',
-            'commandOptions'        => ' - o7 - strip all',
+            'commandPath'           => ' /usr/bin/optipng',
+            'commandOptions'        => '-o7 -strip all',
             'commandOutputFileFlag' => '',
         ],
         'pngcrush'  => [
-            'commandPath'           => ' / usr / bin / pngcrush',
-            'commandOptions'        => ' - brute - ow',
+            'commandPath'           => '/usr/bin/pngcrush',
+            'commandOptions'        => '-brute -ow',
             'commandOutputFileFlag' => '',
         ],
         'pngquant'  => [
-            'commandPath'           => ' / usr / bin / pngquant',
+            'commandPath'           => '/usr/bin/pngquant',
             'commandOptions'        => '--strip--skip -if-larger',
             'commandOutputFileFlag' => '',
         ],
         // svg optimizers
         'svgo'      => [
-            'commandPath'           => ' / usr / bin / svgo',
+            'commandPath'           => '/usr/bin/svgo',
             'commandOptions'        => '',
             'commandOutputFileFlag' => '',
         ],
         // gif optimizers
         'gifsicle'  => [
-            'commandPath'           => ' / usr / bin / gifsicle',
-            'commandOptions'        => ' - O3 - k 256',
+            'commandPath'           => '/usr/bin/gifsicle',
+            'commandOptions'        => '-O3 -k 256',
             'commandOutputFileFlag' => '',
         ],
     ],
@@ -157,10 +157,10 @@ return [
     'imageVariantCreators' => [
         // webp variant creator
         'cwebp' => [
-            'commandPath'           => ' / usr / bin / cwebp',
+            'commandPath'           => '/usr/bin/cwebp',
             'commandOptions'        => '',
-            'commandOutputFileFlag' => ' - o',
-            'commandQualityFlag'    => ' - q',
+            'commandOutputFileFlag' => '-o',
+            'commandQualityFlag'    => '-q',
             'imageVariantExtension' => 'webp',
         ],
     ],
