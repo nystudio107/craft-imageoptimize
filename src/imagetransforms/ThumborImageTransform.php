624825fd4d61b8e5c87c16f90ba35e7d756d28e7
Tim Kelty <tim@fusionary.com>
2018-10-08 11:09:59 -0400

    Initial resizing

--- src/imagetransforms/ThumborImageTransform.php
+++ src/imagetransforms/ThumborImageTransform.php
@@ -17,8 +17,6 @@ use craft\helpers\ArrayHelper;
 use craft\helpers\UrlHelper;
 use craft\models\AssetTransform;
 
-use Thumbor\Url\Builder;
-use Thumbor\Url\BuilderFactory;
 use Psr\Http\Message\ResponseInterface;
 
 use Craft;
@@ -47,6 +45,31 @@ class ThumborImageTransform extends ImageTransform implements ImageTransformInte
      */
     public static function getTransformUrl(Asset $asset, $transform, array $params = [])
     {
+        $assetUri = self::getAssetUri($asset);
+
+        if ($asset->volume instanceof craft\awss3\Volume) {
+            $assetUri = $asset->volume->subfolder.'/'.$asset->path;
+        }
+
+        $baseUrl = $params['baseUrl'];
+        $securityKey = $params['securityKey'] ?: null;
+        $builder = \Thumbor\Url\Builder::construct($baseUrl, $securityKey, $assetUri);
+
+        // exit(var_dump($transform));
+
+        if ($transform->mode === 'fit') {
+            $builder->fitIn($transform->width, $transform->height);
+        } elseif ($transform->mode === 'stretch') {
+            // TODO: not sure?
+            // $builder->resize($transform->width, $transform->height);
+        } else {
+            $builder->resize($transform->width, $transform->height);
+            $builder->addFilter('focal', self::getFocalPoint($asset));
+        }
+
+
+
+        return (string) $builder;
     }
 
     /**
@@ -56,6 +79,7 @@ class ThumborImageTransform extends ImageTransform implements ImageTransformInte
      */
     public static function getWebPUrl(string $url): string
     {
+        return $url;
     }
 
     /**
@@ -84,5 +108,32 @@ class ThumborImageTransform extends ImageTransform implements ImageTransformInte
      */
     public static function getTransformParams(): array
     {
+        $settings = ImageOptimize::$plugin->getSettings();
+        $params = [
+            'baseUrl' => $settings->thumborBaseUrl,
+            'securityKey' => $settings->thumborSecurityKey,
+        ];
+
+        return $params;
+    }
+
+    /**
+     * @return string
+     */
+    protected static function getFocalPoint(Asset $asset)
+    {
+        $focalPoint = $asset->getFocalPoint();
+        $x = $focalPoint['x'] * $asset->width;
+        $y = $focalPoint['y'] * $asset->height;
+
+        return implode('', [
+            $x - 1,
+            'x',
+            $y - 1,
+            ':',
+            $x + 1,
+            'x',
+            $y + 1,
+        ]);
     }
 }
