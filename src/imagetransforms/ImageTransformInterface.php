e3c86865a0974e450656dc4f81e298ff244f92e0
Andrew Welch <andrew@nystudio107.com>
2018-12-28 14:53:30 -0500

    Document the interface
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/imagetransforms/ImageTransformInterface.php
+++ src/imagetransforms/ImageTransformInterface.php
@@ -25,6 +25,9 @@ interface ImageTransformInterface extends SavableComponentInterface
     // =========================================================================
 
     /**
+     * Return an array that contains the template root and corresponding file
+     * system directory for the Image Transform's templates
+     *
      * @return array
      * @throws \ReflectionException
      */
@@ -34,6 +37,8 @@ interface ImageTransformInterface extends SavableComponentInterface
     // =========================================================================
 
     /**
+     * Return a URL to a transformed images
+     *
      * @param Asset               $asset
      * @param AssetTransform|null $transform
      * @param array               $params
@@ -43,6 +48,8 @@ interface ImageTransformInterface extends SavableComponentInterface
     public function getTransformUrl(Asset $asset, $transform, array $params = []);
 
     /**
+     * Return a URL to the webp version of the transformed image
+     *
      * @param string              $url
      * @param Asset               $asset
      * @param AssetTransform|null $transform
@@ -53,6 +60,8 @@ interface ImageTransformInterface extends SavableComponentInterface
     public function getWebPUrl(string $url, Asset $asset, $transform, array $params = []): string;
 
     /**
+     * Return the URL that should be used to purge the Asset
+     *
      * @param Asset $asset
      * @param array $params
      *
@@ -61,6 +70,8 @@ interface ImageTransformInterface extends SavableComponentInterface
     public function getPurgeUrl(Asset $asset, array $params = []);
 
     /**
+     * Purge the URL from the service's cache
+     *
      * @param string $url
      * @param array  $params
      *
@@ -69,6 +80,8 @@ interface ImageTransformInterface extends SavableComponentInterface
     public function purgeUrl(string $url, array $params = []): bool;
 
     /**
+     * Return the URI to the asset
+     *
      * @param Asset $asset
      *
      * @return mixed
@@ -76,11 +89,15 @@ interface ImageTransformInterface extends SavableComponentInterface
     public function getAssetUri(Asset $asset);
 
     /**
+     * Prefetch the remote file to prime the cache
+     *
      * @param string $url
      */
     public function prefetchRemoteFile($url);
 
     /**
+     * Get the parameters needed for this transform
+     *
      * @return array
      */
     public function getTransformParams(): array;
