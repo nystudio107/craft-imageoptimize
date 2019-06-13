b7d9d510a81db23b88573cdd9ef5104b04bd078d
Andrew Welch <andrew@nystudio107.com>
2019-06-13 12:44:14 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/imageoptimize/dist/js/styles.js
+++ src/assetbundles/imageoptimize/dist/js/styles.js
@@ -2,9 +2,9 @@
  * @project        Image Optimize
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Thu, Dec 20, 2018 12:49 AM ET
- * @release        cbf575c3ae313952cca16ad83d84791f3692bb1e [develop]
- * @copyright      Copyright (c) 2018 nystudio107
+ * @build          Thu, Jun 13, 2019 4:40 PM ET
+ * @release        c24cd1572fcf45b5fb937bf5e68ab373e935f4a8 [develop]
+ * @copyright      Copyright (c) 2019 nystudio107
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{5:function(n,w,o){}}]);
