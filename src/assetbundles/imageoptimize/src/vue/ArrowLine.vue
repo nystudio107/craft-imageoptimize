9d8e6a0d39dddfc275e4751af9ec5702c6a15b25
Andrew Welch <andrew@nystudio107.com>
2021-01-16 13:47:06 -0500

    Initial arrow component
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/imageoptimize/src/vue/ArrowLine.vue
+++ src/assetbundles/imageoptimize/src/vue/ArrowLine.vue
@@ -1,23 +1,32 @@
 <template>
-    <main>
-    </main>
+  <svg :width="width" height="20px">
+    <marker :id="`${ name } startarrow`" :markerWidth="markerWidth" :markerHeight="markerHeight"
+            :refX="markerWidth" :refY="markerHeight / 2" orient="auto">
+      <polygon :points="`${ markerWidth } 0, ${ markerWidth } ${ markerHeight }, 0 ${ markerHeight / 2 }`" :fill="strokeColor" />
+    </marker>
+    <marker :id="`${ name }}endarrow`" :markerWidth="markerWidth" :markerHeight="markerHeight"
+            refX="0" :refY="markerHeight / 2" orient="auto" markerUnits="strokeWidth">
+      <polygon points="`0 0, ${ markerWidth } ${ markerHeight / 2 }, 0 ${ markerHeight }`" :fill="strokeColor" />
+    </marker>
+    <line :x1="markerWidth * 2" :y1="lineY" :x2="width - (markerWidth * 2)" :y2="lineY" :stroke="strokeColor" stroke-width="2" marker-end="`url(#${ name }endarrow)`" marker-start="`url(#${ name }startarrow)`" />
+    <text :x="width / 2" :y="lineY + 4" text-anchor="middle" fill="white" stroke="white" stroke-width="5">{{ width }}w</text>
+    <text :x="width / 2" :y="lineY + 4" text-anchor="middle" :fill="strokeColor">{{ width }}w</text>
+  </svg>
 </template>
 
 <script>
-    import VueConfetti from 'vue-confetti';
-
-    Vue.use(VueConfetti);
-
     export default {
-        mounted: function() {
-            this.$confetti.start({
-                shape: 'rect',
-                colors: ['DodgerBlue', 'OliveDrab', 'Gold', 'pink', 'SlateBlue', 'lightblue', 'Violet', 'PaleGreen', 'SteelBlue', 'SandyBrown', 'Chocolate', 'Crimson']
-            });
-            setTimeout(() => {
-                this.$confetti.stop();
-            }, 5000);
+        props: {
+          name: 'woof',
+          width: 200,
         },
+        data: () => ({
+          strokeColor: '#AAA',
+          fillColor: '#DDD',
+          markerWidth: 4,
+          markerHeight: 4,
+          lineY: 12,
+        }),
         methods: {
         }
     }
