64494e9df6d53d10dd85a4998e4dd2ce808ffe4a
Andrew Welch <andrew@nystudio107.com>
2021-01-15 23:13:42 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/imageoptimize/dist/js/imageoptimize.js
+++ src/assetbundles/imageoptimize/dist/js/imageoptimize.js
@@ -2,9 +2,9 @@
  * @project        imageoptimize
  * @name           imageoptimize.js
  * @author         Andrew Welch
- * @build          Wed Dec 09 2020 05:21:12 GMT+0000 (Coordinated Universal Time)
- * @copyright      Copyright (c) 2020 ©2020 nystudio107.com
+ * @build          Sat Jan 16 2021 03:56:20 GMT+0000 (Coordinated Universal Time)
+ * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
-(self.webpackChunkimageoptimize=self.webpackChunkimageoptimize||[]).push([[438],{803:function(e,i,p){"use strict";p.p}},0,[[803,666]]]);
+(self.webpackChunkimageoptimize=self.webpackChunkimageoptimize||[]).push([[438],{7803:function(e,i,p){"use strict";p.p}},0,[[7803,666]]]);
 //# sourceMappingURL=imageoptimize.js.map
