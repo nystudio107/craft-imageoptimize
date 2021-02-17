c89f07abb58626f4fd69ebb876f136e79fea15f9
Andrew Welch <andrew@nystudio107.com>
2021-02-16 23:57:30 -0500

    Cleanup imports
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/variables/ManifestVariable.php
+++ src/variables/ManifestVariable.php
@@ -4,14 +4,17 @@ namespace nystudio107\imageoptimize\variables;
 
 use nystudio107\imageoptimize\helpers\Manifest as ManifestHelper;
 
+use yii\base\InvalidConfigException;
+use yii\web\NotFoundHttpException;
+
 class ManifestVariable
 {
     /**
      * Get the passed in JS modules from the manifest, and register them in the current Craft view
      *
      * @param array $modules
-     * @throws \yii\base\InvalidConfigException
-     * @throws \yii\web\NotFoundHttpException
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
      */
     public function registerJsModules(array $modules)
     {
@@ -22,8 +25,8 @@ class ManifestVariable
      * Get the passed in CS modules from the manifest, and register them in the current Craft view
      *
      * @param array $modules
-     * @throws \yii\base\InvalidConfigException
-     * @throws \yii\web\NotFoundHttpException
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
      */
     public function registerCssModules(array $modules)
     {
