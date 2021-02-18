ed393ddc8d11d1e396d8527e68c6fef25d0b6366
Andrew Welch <andrew@nystudio107.com>
2021-02-18 08:34:24 -0500

    Fix errant double-slash
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- buildchain/webpack-settings/app.settings.js
+++ buildchain/webpack-settings/app.settings.js
@@ -7,7 +7,7 @@ const path = require('path');
 // settings
 module.exports = {
     alias: {
-        '@css': path.resolve('../src/assetbundles/imageoptimize/src//css'),
+        '@css': path.resolve('../src/assetbundles/imageoptimize/src/css'),
         '@img': path.resolve('../src/assetbundles/imageoptimize/src/img'),
         '@js': path.resolve('../src/assetbundles/imageoptimize/src/js'),
         '@vue': path.resolve('../src/assetbundles/imageoptimize/src/vue'),
