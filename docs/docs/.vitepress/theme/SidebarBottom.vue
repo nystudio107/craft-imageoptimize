f49e1f7b1bf833d0ff33d7109e71ed85df33b1a8
Andrew Welch <andrew@nystudio107.com>
2021-06-09 22:22:05 -0400

    Switched documentation system to VitePress
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ docs/docs/.vitepress/theme/SidebarBottom.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="nys-footer">
+    <span>Brought to you by:</span>
+    <a
+      href="https://github.com/sponsors/khalwat"
+      target="_blank"
+      rel="noopener"
+    >
+    <img
+        src="/resources/img/nys-logo.svg"
+        aria-label="nystudio107 logo"
+        />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarBottom"
+}
+</script>
+
+<style scoped>
+.nys-footer {
+  padding: 0 1.5rem 2rem;
+  font-size: 0.8rem;
+}
+
+.nys-footer img {
+  box-sizing: border-box;
+  max-width: 200px;
+  height: 80px;
+  display: block;
+  margin: 1rem 0;
+  padding-left: 0.7rem;
+}
+</style>
---
+++ docs/docs/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3002,
+    strictPort: true,
+  }
+});
