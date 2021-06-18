4810447c2b65714c2da4fe0ffc270200976b67d4
Andrew Welch <andrew@keluli.local>
2021-06-18 10:04:19 -0400

    Add Google Analytics to docs

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -3,6 +3,13 @@ module.exports = {
     description: 'Documentation for the ImageOptimize plugin',
     base: '/docs/image-optimize/',
     lang: 'en-US',
+    head: [
+        [
+            'script',
+            {},
+            "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-69117511-1', 'auto');ga('require', 'displayfeatures');ga('send', 'pageview');"
+        ],
+    ],
     themeConfig: {
         repo: 'nystudio107/craft-imageoptimize',
         docsDir: 'docs/docs',
