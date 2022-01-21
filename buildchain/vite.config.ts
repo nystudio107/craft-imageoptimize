f4c0d2976142bfc1df60bf0f6bfe2a67cdc3f276
Andrew Welch <andrew@nystudio107.com>
2022-01-20 20:47:30 -0500

    feat: Switch to vite.config.ts & defineConfig() & add `vite-plugin-manifest-sri` to generate asset integrity hashes

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -1,14 +1,16 @@
+import {defineConfig} from 'vite';
 import {createVuePlugin} from 'vite-plugin-vue2'
 import ViteRestart from 'vite-plugin-restart';
 import {viteExternalsPlugin} from 'vite-plugin-externals'
 import viteCompression from 'vite-plugin-compression';
+import manifestSRI from 'vite-plugin-manifest-sri';
 import {visualizer} from 'rollup-plugin-visualizer';
 import eslintPlugin from 'vite-plugin-eslint';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
-import path from 'path';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default ({command}) => ({
+export default defineConfig((({command}) => ({
   base: command === 'serve' ? '' : '/dist/',
   build: {
     emptyOutDir: true,
@@ -43,6 +45,7 @@ export default ({command}) => ({
     viteCompression({
       filter: /\.(js|mjs|json|css|map)$/i
     }),
+    manifestSRI(),
     visualizer({
       filename: '../src/web/assets/dist/stats.html',
       template: 'treemap',
@@ -69,4 +72,4 @@ export default ({command}) => ({
     port: 3001,
     strictPort: true,
   }
-});
+}));
