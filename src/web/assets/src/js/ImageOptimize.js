49f670edf975250cc662d2ec1a25429009eedc05
Andrew Welch <andrew@keluli.local>
2022-06-30 11:26:06 -0400

    refactor: Ensure entry script HMR

--- src/web/assets/src/js/ImageOptimize.js
+++ src/web/assets/src/js/ImageOptimize.js
@@ -1,2 +1,9 @@
 // Import our CSS
 import '@/css/app.css';
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  });
+}
--- src/web/assets/src/js/Welcome.js
+++ src/web/assets/src/js/Welcome.js
@@ -2,12 +2,17 @@ import Vue from 'vue';
 import ConfettiParty from '@/vue/ConfettiParty.vue';
 
 new Vue({
-    el: "#cp-nav-content",
-    components: {
-        ConfettiParty,
-    },
-    data: {
-    },
-    methods: {
-    },
+  el: "#cp-nav-content",
+  components: {
+    ConfettiParty,
+  },
+  data: {},
+  methods: {},
 });
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  });
+}
