be08ef242fdd69e67df7839b2b522d25cc71ce78
Andrew Welch <andrew@nystudio107.com>
2023-01-28 15:33:53 -0500

    chore: Move src dir to the buildchain to avoid path issues

---
+++ buildchain/src/js/@types/shims.d.ts
@@ -0,0 +1,10 @@
+declare module "seomatic";
+declare module "seomatic-meta";
+declare module "content-seo";
+declare module "dashboard";
+
+declare module "*.vue" {
+  import Vue from 'vue';
+  export default Vue;
+}
+declare let Craft: unknown;
