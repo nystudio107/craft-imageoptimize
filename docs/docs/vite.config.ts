24e21c772283c0b796a394b5af763a18d2bddeab
Andrew Welch <andrew@nystudio107.com>
2023-01-29 20:19:53 -0500

    refactor: Makefile more consistent

--- docs/docs/vite.config.ts
+++ docs/docs/vite.config.ts
@@ -1,17 +1,18 @@
 import {defineConfig} from 'vite'
 import SitemapPlugin from 'rollup-plugin-sitemap'
 import VitePressConfig from './.vitepress/config'
-import {SidebarGroup, SidebarItem} from "vitepress/types/default-theme";
+import {DefaultTheme} from "vitepress/types/default-theme";
+import SidebarItem = DefaultTheme.SidebarItem;
 
 const docsSiteBaseUrl = 'https://nystudio107.com'
 const docsBaseUrl = new URL(VitePressConfig.base!, docsSiteBaseUrl).href.replace(/\/$/, '') + '/'
-const siteMapRoutes = VitePressConfig.themeConfig?.sidebar?.map((group: SidebarGroup) => {
-  return group.items.map((items: SidebarItem) => ({
-    path: items.link.replace(/^\/+/, ''),
+const siteMapRoutes = VitePressConfig.themeConfig?.sidebar?.map((group: SidebarItem) => {
+  return group.items!.map((items: SidebarItem) => ({
+    path: items.link!.replace(/^\/+/, ''),
     name: items.text
   }));
 }).reduce((prev: SidebarItem[], curr: SidebarItem[]) => {
-  return prev.concat(curr)
+  return prev.concat(curr);
 });
 
 // https://vitejs.dev/config/
@@ -25,7 +26,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
-    port: 3002,
+    port: parseInt(process.env.DOCS_DEV_PORT),
     strictPort: true,
   }
 });
