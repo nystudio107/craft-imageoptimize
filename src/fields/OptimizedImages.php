ad1149431001a24b323c9e5f8cf8bb25f20e1219
Andrew Welch <andrew@nystudio107.com>
2023-03-14 16:20:05 -0400

    refactor: trigger off of upload instead

--- src/fields/OptimizedImages.php
+++ src/fields/OptimizedImages.php
@@ -237,7 +237,7 @@ class OptimizedImages extends Field
              */
             if (
                 !$request->isConsoleRequest
-                && ($pathInfo === 'actions/entries/save-entry' || $pathInfo === 'actions/assets/save-image')
+                && ($pathInfo === 'actions/entries/save-entry' || $pathInfo === 'actions/assets/upload')
                 && PluginHelper::blitzWarmingActive()
             ) {
                 try {
