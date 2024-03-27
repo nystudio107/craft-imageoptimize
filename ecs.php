5905548a2a928d3ce1f691e0d0fe70b5e155356d
Andrew Welch <andrew@nystudio107.com>
2024-03-27 19:54:32 -0400

    feat: Add `phpstan` and `ecs` code linting

---
+++ ecs.php
@@ -0,0 +1,13 @@
+<?php
+
+use craft\ecs\SetList;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function(ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __FILE__,
+    ]);
+    $ecsConfig->parallel();
+    $ecsConfig->sets([SetList::CRAFT_CMS_3]);
+};
