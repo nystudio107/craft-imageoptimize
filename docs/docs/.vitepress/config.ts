522363b063ad5e25a52f06f6726046cb48626658
Andrew Welch <andrew@nystudio107.com>
2024-03-30 13:07:23 -0400

    docs: Fix links

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     ],
     logo: '/img/plugin-logo.svg',
     editLink: {
-      pattern: 'https://github.com/nystudio107/craft-imageoptimize/edit/develop/docs/docs/:path',
+      pattern: 'https://github.com/nystudio107/craft-imageoptimize/edit/develop-v4/docs/docs/:path',
       text: 'Edit this page on GitHub'
     },
     algolia: {
@@ -51,7 +51,7 @@ export default defineConfig({
         text: 'v4', items: [
           {text: 'v5', link: 'https://nystudio107.com/docs/image-optimize/'},
           {text: 'v4', link: '/'},
-          {text: 'v3', link: 'https://nystudio107.com/docs/image-optimize/v1/'},
+          {text: 'v1', link: 'https://nystudio107.com/docs/image-optimize/v1/'},
         ],
       },
     ],
