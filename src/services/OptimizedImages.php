853fa9bd05549835f07bd4cd0af635438b728305
Andrew Welch <andrew@nystudio107.com>
2026-02-14 13:08:48 -0500

    fix: Prevent Duplicate optimization queue jobs created when resaving assets ([#433](https://github.com/nystudio107/craft-imageoptimize/issues/433))

--- src/services/OptimizedImages.php
+++ src/services/OptimizedImages.php
@@ -49,8 +49,13 @@ class OptimizedImages extends Component
     // Constants
     // =========================================================================
 
-    // Public Properties
+    // Protected Properties
     // =========================================================================
+
+    /**
+     * @var array IDs of assets that have already been added to the queue
+     */
+    protected static array $processedIds = [];
 
     // Public Methods
     // =========================================================================
@@ -395,6 +400,11 @@ class OptimizedImages extends Component
      */
     public function resaveAsset(int $id, bool $force = false): void
     {
+        if (in_array($id, self::$processedIds, true)) {
+            return;
+        }
+        self::$processedIds[] = $id;
+
         $queue = Craft::$app->getQueue();
         $jobId = $queue->push(new ResaveOptimizedImages([
             'description' => Craft::t('image-optimize', 'Optimizing image id {id}', ['id' => $id]),
